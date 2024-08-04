eb21b9e14824a9933f3e818dca18fcf8a51f0e50
Bde-meij <92989324+Bde-meij@users.noreply.github.com>
2024-08-04 10:06:56 +0200

    Created test main

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "ft_printf.h"
+#include "stdio.h"
+
+int main()
+{
+    void *ptr;
+    ft_printf("\nfakePrint: characters printed = %i\n", ft_printf("fakePrint: %c, %s, pointer: %p, integer: %i, hexadec: %x, HEXADEC: %X", 'a', "string", ptr, 15, 15, 15));
+    printf("\nrealPrint: characters printed = %i\n", printf("realPrint: %c, %s, pointer: %p, integer: %i, hexadec: %x, HEXADEC: %X", 'a', "string", ptr, 15, 15, 15));
+
+    ft_printf("%% %%% %%%   %% %\n");
+    printf("%% %%% %%%   %% %\n");
+}
