ab92f88bc03e0c7948589341d1bc51ac5fa2c58f
unknown <bde-meij@student.codam.nl>
2024-08-09 17:00:44 +0200

    ssh check

--- main.c
+++ main.c
@@ -7,6 +7,6 @@ int main()
     ft_printf("\nfakePrint: characters printed = %i\n", ft_printf("fakePrint: %c, %s, pointer: %p, integer: %i, hexadec: %x, HEXADEC: %X", 'a', "string", ptr, 15, 15, 15));
     printf("\nrealPrint: characters printed = %i\n", printf("realPrint: %c, %s, pointer: %p, integer: %i, hexadec: %x, HEXADEC: %X", 'a', "string", ptr, 15, 15, 15));
 
-    ft_printf("%%%%%%%%%%\n");
-    printf("%%%%%%%%%%\n");
+    ft_printf("%%%%%%%%%%%%\n");
+    printf("%%%%%%%%%%%%\n");
 }
